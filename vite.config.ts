c003339fc0e018f5ab11bd84681e041039e63d65
ajstworkspace-lgtm <ajstworkspace@gmail.com>
2026-05-25 08:39:17 -0800

    Fix 2  Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/ajst-copy-image/",  // <-- ADD THIS LINE (change to your repo name)
+export default defineConfig({
+  plugins: [react()],
+  base: '/ajst-copy-image/', 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
-  },
-}));
+  }, // مهم جداً
+})
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({  }) => ({
+//   plugins: [react()],
+//   base: "/ajst-copy-image/",  // <-- ADD THIS LINE (change to your repo name)
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+//   },
+// }));
